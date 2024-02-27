36a9b43f291804dfab92580c55f0176c736521d8
Zeldack974 <zeldak974@gmail.com>
2024-02-27 16:39:19 +0400

    feat: EnergyCell ability (beta) feat: buff PuffBall feat: custom data

---
+++ CustomData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ArenaSlugcatsConfigurator
+{
+    public static class CustomDataManager
+    {
+        public static readonly ConditionalWeakTable<object, CustomData> customData = new ConditionalWeakTable<object, CustomData>();
+        public static T GetCustomData<T>(this object obj) where T : CustomData, new()
+        {
+            return (T)customData.GetValue(obj, _ => new T { owner = obj });
+        }
+
+    }
+    public class CustomData
+    {
+        public object owner;
+        public CustomData(object obj)
+        {
+
+            owner = obj;
+        }
+    }
+
+    public class RoomCustomData : CustomData
+    {
+        public int frame;
+
+        // Constructeur par défaut
+        public RoomCustomData() : base(null) { }
+
+        // Autres constructeurs si nécessaire
+        public RoomCustomData(object obj) : base(obj) { }
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -68,9 +68,11 @@ namespace ArenaSlugcatsConfigurator
             On.Player.Regurgitate += Player_Regurgitate;
             On.Player.ClassMechanicsArtificer += Player_ClassMechanicsArtificer;
 
+            On.Room.Loaded += Room_Loaded;
+            On.Room.Update += Room_Update;
+
             On.ArenaSitting.SessionEnded += ArenaSitting_SessionEnded;
             On.ArenaGameSession.SpawnPlayers += ArenaGameSession_SpawnPlayers;
-            On.Room.Loaded += Room_Loaded;
             On.ArenaGameSession.Update += ArenaGameSession_Update;
             On.Menu.PlayerResultBox.ctor += PlayerResultBox_ctor;
             On.PlayerGraphics.DrawSprites += PlayerGraphics_DrawSprites;
@@ -82,6 +84,129 @@ namespace ArenaSlugcatsConfigurator
             On.Spear.HitSomething += Spear_HitSomething;
 
             On.PuffBall.Explode += PuffBall_Explode;
+
+            //On.MoreSlugcats.EnergyCell.Update += EnergyCell_Update;
+            On.MoreSlugcats.EnergyCell.DrawSprites += EnergyCell_DrawSprites;
+            On.MoreSlugcats.EnergyCell.Use += EnergyCell_Use;
+        }
+
+        private void EnergyCell_Use(On.MoreSlugcats.EnergyCell.orig_Use orig, EnergyCell self, bool forced)
+        {
+            orig(self, forced);
+            if (self.usingTime == 600f) self.usingTime *= 2f;
+        }
+
+        private void EnergyCell_DrawSprites(On.MoreSlugcats.EnergyCell.orig_DrawSprites orig, EnergyCell self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos)
+        {
+            orig(self, sLeaser, rCam, timeStacker, camPos);
+            sLeaser.sprites[3].scale = 15f * 1f;
+        }
+
+        private void Room_Update(On.Room.orig_Update orig, Room self)
+        {
+            orig(self);
+
+            if (!self.game.IsArenaSession || IsChallengeGameSession(self.game))
+            {
+                return;
+            }
+
+            RoomCustomData customData = self.GetCustomData<RoomCustomData>();
+            customData.frame++;
+
+            //if (customData.frame % 20 > 0)
+            //{
+            //    return;
+            //}
+
+            List<EnergyCell> energyCells = new List<EnergyCell>();
+            for (int j = 0; j < self.physicalObjects.Length; j++)
+            {
+                for (int k = 0; k < self.physicalObjects[j].Count; k++)
+                {
+                    if (self.physicalObjects[j][k] is EnergyCell)
+                    {
+                        energyCells.Add(self.physicalObjects[j][k] as EnergyCell);
+                    }
+                }
+            }
+
+
+            for (int j = 0; j < self.physicalObjects.Length; j++)
+            {
+                for (int k = 0; k < self.physicalObjects[j].Count; k++)
+                {
+                    PhysicalObject obj = self.physicalObjects[j][k];
+
+                    bool notProtected = obj is not Creature || (obj as Creature).grasps == null || (obj as Creature).grasps[0] == null || (obj as Creature).grasps[0].grabbed is not EnergyCell || ((obj as Creature).grasps[0].grabbed as EnergyCell).usingTime == 0f;
+
+                    if (!obj.slatedForDeletetion && notProtected)
+                    {
+                        foreach (EnergyCell energyCell in energyCells)
+                        {
+                            bool doBreak = false;
+
+
+
+                            if (!energyCell.slatedForDeletetion)
+                            {
+                                if (energyCell.usingTime > 0f)
+                                {
+                                    (energyCell as PhysicalObject).gravity = 0.45f;
+                                    //energyCell.firstChunk.vel = energyCell.firstChunk.vel.normalized * Math.Min(energyCell.firstChunk.vel.magnitude, 10f);
+                                }
+
+                                if (obj is not EnergyCell)
+                                {
+                                    if (energyCell.usingTime > 0f)
+                                    {
+                                        foreach (BodyChunk chuck in obj.bodyChunks)
+                                        {
+                                            float dist = Vector2.Distance(energyCell.firstChunk.pos, chuck.pos);
+                                            if (dist < 110f * 1f) // 75
+                                            {
+                                                Debug.Log("no gravity to " + obj);
+
+                                                if (obj is Creature)
+                                                {
+                                                    energyCell.firstChunk.vel += (chuck.pos - energyCell.firstChunk.pos).normalized * 0.25f;
+                                                }
+
+
+                                                if (obj is Player)
+                                                {
+                                                    (obj as Player).customPlayerGravity = 0f;
+                                                    (obj as Player).animation = Player.AnimationIndex.ZeroGSwim;
+                                                }
+                                                else
+                                                {
+                                                    chuck.vel.y += obj.gravity;
+                                                    Debug.Log("max: " + chuck.mass * 200);
+                                                    chuck.vel = chuck.vel.normalized * Math.Min(chuck.vel.magnitude, chuck.mass * 400);
+                                                }
+
+                                                doBreak = true;
+                                                //blackList.Add(chuck);
+                                            }
+
+                                        }
+                                    }
+                                    else
+                                    {
+                                        if (obj is Player && ((obj as Player).animation == Player.AnimationIndex.ZeroGSwim || (obj as Player).animation == Player.AnimationIndex.ZeroGPoleGrab))
+                                        {
+                                            (obj as Player).customPlayerGravity = 0.9f;
+                                            (obj as Player).animation = Player.AnimationIndex.None;
+                                        }
+                                    }
+                                }
+                            }
+
+                            if (doBreak) break;
+                        }
+                    }
+                }
+            }
         }
 
         private void PuffBall_Explode(On.PuffBall.orig_Explode orig, PuffBall self)
@@ -102,7 +227,7 @@ namespace ArenaSlugcatsConfigurator
                     //ConsoleWrite($"try stun slugcat: {self.room.abstractRoom.creatures[i].realizedCreature.Template.type == CreatureTemplate.Type.Slugcat}, {Vector2.Distance(self.firstChunk.pos, self.room.abstractRoom.creatures[i].realizedCreature.mainBodyChunk.pos)}");
                     if (self.room.abstractRoom.creatures[i].realizedCreature.Template.type == CreatureTemplate.Type.Slugcat && Custom.DistLess(self.firstChunk.pos, self.room.abstractRoom.creatures[i].realizedCreature.mainBodyChunk.pos, (1f + 20f) * 3))
                     {
-                        int stun = Mathf.RoundToInt(20f * Random.value * 2f / Mathf.Lerp(self.room.abstractRoom.creatures[i].realizedCreature.TotalMass, 1f, 0.15f));
+                        int stun = Mathf.RoundToInt(20f * Random.value * 3f / Mathf.Lerp(self.room.abstractRoom.creatures[i].realizedCreature.TotalMass, 1f, 0.15f));
                         self.room.abstractRoom.creatures[i].realizedCreature.Stun(stun);
                         //ConsoleWrite("stun slugcat for " + stun);
                     }
