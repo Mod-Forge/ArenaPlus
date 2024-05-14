fd826539bdced7b2b8566d246eb0f93ff8677b06
Zeldack974 <Zeldak974@gmail.com>
2024-05-14 10:10:47 +0400

    feat: exeptions for player oneshot

--- Freatures/PlayerOneshot.cs
+++ Freatures/PlayerOneshot.cs
@@ -1,10 +1,16 @@
 ﻿
 using System;
+using System.Linq;
 
 namespace ArenaSlugcatsConfigurator.Freatures
 {
     internal static class PlayerOneshot
     {
+        public static string[] expetions = new string[1]
+        { 
+            "Shaw"
+        };
+
         internal static void OnEnable()
         {
             logSource.LogInfo("PlayerOneshot OnEnable");
@@ -14,7 +20,7 @@ namespace ArenaSlugcatsConfigurator.Freatures
         private static bool Spear_HitSomething(On.Spear.orig_HitSomething orig, Spear self, SharedPhysics.CollisionResult result, bool eu)
         {
             bool val = orig(self, result, eu);
-            if (Options.enableSpearOneShot.Value && self.thrownBy != null && self.thrownBy is Player && result.obj != null && result.obj is Player)
+            if (Options.enableSpearOneShot.Value && self.thrownBy != null && self.thrownBy is Player && result.obj != null && result.obj is Player && !expetions.Contains((self.thrownBy as Player).slugcatStats.name.value))
             {
                 (result.obj as Player).Die();
             }
