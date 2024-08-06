4d4d845e7b5e9b8e1fd6634d042991a5f71a8362
Zeldack974 <Zeldak974@gmail.com>
2024-08-06 19:40:48 +0400

    fix: player oneshot now oneshot when the spear is dealing damage

--- Freatures/PlayerOneshot.cs
+++ Freatures/PlayerOneshot.cs
@@ -19,8 +19,13 @@ namespace ArenaSlugcatsConfigurator.Freatures
 
         private static bool Spear_HitSomething(On.Spear.orig_HitSomething orig, Spear self, SharedPhysics.CollisionResult result, bool eu)
         {
+            double heath = 0;
+            if (result.obj != null && result.obj is Player) {
+                heath = (result.obj as Player).playerState.permanentDamageTracking;
+            }
+
             bool val = orig(self, result, eu);
-            if (Options.enableSpearOneShot.Value && self.thrownBy != null && self.thrownBy is Player && result.obj != null && result.obj is Player && !expetions.Contains((self.thrownBy as Player).slugcatStats.name.value))
+            if (Options.enableSpearOneShot.Value && self.thrownBy != null && self.thrownBy is Player && result.obj != null && result.obj is Player && !expetions.Contains((self.thrownBy as Player).slugcatStats.name.value) && heath > (result.obj as Player).playerState.permanentDamageTracking)
             {
                 (result.obj as Player).Die();
             }
