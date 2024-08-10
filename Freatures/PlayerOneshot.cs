14e5b5e29d38bfe5b13a8b59aec0fe635e541d06
Zeldack974 <Zeldak974@gmail.com>
2024-08-10 12:18:01 +0400

    fix: the player one shot for good i hope

--- Freatures/PlayerOneshot.cs
+++ Freatures/PlayerOneshot.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace ArenaSlugcatsConfigurator.Freatures
 {
@@ -11,22 +12,47 @@ namespace ArenaSlugcatsConfigurator.Freatures
             "Gourmand"
         };
 
+        public static ConditionalWeakTable<Weapon, Creature> killtags = new ConditionalWeakTable<Weapon, Creature>();
+
         internal static void OnEnable()
         {
             logSource.LogInfo("PlayerOneshot OnEnable");
             On.Spear.HitSomething += Spear_HitSomething;
+            On.Weapon.Update += Weapon_Update;
+        }
+
+        private static void Weapon_Update(On.Weapon.orig_Update orig, Weapon self, bool eu)
+        {
+            orig(self, eu);
+            if (self.thrownBy != null)
+            {
+                if (killtags.TryGetValue(self, out var killTag) && killTag != self.thrownBy)
+                {
+                    killtags.Remove(self);
+                }
+
+                if (self.thrownBy is Player)
+                {
+                    killtags.Add(self, self.thrownBy as Player);
+                }
+            }
         }
 
         private static bool Spear_HitSomething(On.Spear.orig_HitSomething orig, Spear self, SharedPhysics.CollisionResult result, bool eu)
         {
-            double heath = 0;
+            double damage = 0;
             if (result.obj != null && result.obj is Player) {
-                heath = (result.obj as Player).playerState.permanentDamageTracking;
+                ConsoleWrite("spear hiting a player");
+                damage = (result.obj as Player).playerState.permanentDamageTracking;
             }
 
             bool val = orig(self, result, eu);
-            if (Options.enableSpearOneShot.Value && self.thrownBy != null && self.thrownBy is Player && result.obj != null && result.obj is Player && !expetions.Contains((self.thrownBy as Player).slugcatStats.name.value) && heath > (result.obj as Player).playerState.permanentDamageTracking)
+            if (result.obj != null && result.obj is Player)  ConsoleWrite("damage taked: " + ((result.obj as Player).playerState.permanentDamageTracking - damage));
+
+            self.thrownBy ??= killtags.TryGetValue(self, out var killTag) ? killTag : null;
+            if (Options.enableSpearOneShot.Value && self.thrownBy != null && self.thrownBy is Player && result.obj != null && result.obj is Player && !expetions.Contains((self.thrownBy as Player).slugcatStats.name.value) && damage < (result.obj as Player).playerState.permanentDamageTracking)
             {
+                ConsoleWrite("force kill player");
                 (result.obj as Player).Die();
             }
             return val;
