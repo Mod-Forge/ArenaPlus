ea5ac6f6cc6f8f3d49bd2820db3d969ca09eafdb
Zeldack974 <115973286+Zeldack974@users.noreply.github.com>
2025-01-19 14:43:12 +0400

    feat: KeybordPause feature

---
+++ src/Features/UI/KeybordPause.cs
@@ -0,0 +1,27 @@
+﻿using ArenaPlus.Lib;
+using ArenaPlus.Utils;
+using Menu;
+using RWCustom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ArenaPlus.Features.UI
+{
+    [ImmutableFeature]
+    internal class KeybordPause : ImmutableFeature
+    {
+        protected override void Register()
+        {
+            On.RWInput.CheckPauseButton_int_bool += RWInput_CheckPauseButton_int_bool;
+        }
+
+        private bool RWInput_CheckPauseButton_int_bool(On.RWInput.orig_CheckPauseButton_int_bool orig, int playerNumber, bool inMenu)
+        {
+            return orig(playerNumber, inMenu) || Input.GetKey(KeyCode.Escape);
+        }
+    }
+}
