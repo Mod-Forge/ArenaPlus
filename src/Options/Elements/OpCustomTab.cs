718e9e229dca707208233fdb771eb96e0a640678
ddemile <emilienhoarau10@gmail.com>
2025-01-19 13:58:42 +0400

    feat: Complementary element system for features

---
+++ src/Options/Elements/OpCustomTab.cs
@@ -0,0 +1,15 @@
+﻿using Menu.Remix.MixedUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.PlayerLoop;
+
+namespace ArenaPlus.Options.Elements
+{
+    internal class OpCustomTab(OptionInterface owner, string name = "") : OpTab(owner, name)
+    {
+        internal virtual void Update() { }
+    }
+}
