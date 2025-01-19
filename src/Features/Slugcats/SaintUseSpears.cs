21a3751e685595f8060310024195dd66f02ca05b
Zeldack974 <115973286+Zeldack974@users.noreply.github.com>
2025-01-19 19:56:43 +0400

    feat: saint spin can throw spear

--- src/Features/Slugcats/SaintUseSpears.cs
+++ src/Features/Slugcats/SaintUseSpears.cs
@@ -1,9 +1,12 @@
 ﻿using ArenaPlus.Lib;
+using MoreSlugcats;
+using RWCustom;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace ArenaPlus.Features.Slugcats
 {
@@ -17,11 +20,34 @@ namespace ArenaPlus.Features.Slugcats
     {
         protected override void Register()
         {
-            throw new NotImplementedException("TODO: Implement that");
+            On.Player.TossObject += Player_TossObject;
         }
 
         protected override void Unregister()
         {
+            On.Player.TossObject -= Player_TossObject;
+        }
+
+        private void Player_TossObject(On.Player.orig_TossObject orig, Player self, int grasp, bool eu)
+        {
+            if (self.SlugCatClass == MoreSlugcatsEnums.SlugcatStatsName.Saint && self.grasps[grasp] != null && self.grasps[grasp].grabbed is Spear)
+            {
+                IntVector2 intVector = new IntVector2(self.ThrowDirection, 0);
+                if (self.animation == Player.AnimationIndex.Flip && (self.input[0].y < 0 || (MMF.cfgUpwardsSpearThrow.Value && self.input[0].y > 0)))
+                {
+                    intVector = new IntVector2(0, MMF.cfgUpwardsSpearThrow.Value ? self.input[0].y : -1);
+                }
+                else
+                {
+                    intVector = new IntVector2(self.ThrowDirection, 0);
+                }
+
+
+                self.slugcatStats.throwingSkill = 2;
+                (self.grasps[grasp].grabbed as Weapon).Thrown(self, self.firstChunk.pos + intVector.ToVector2() * 10f + new Vector2(0f, 4f), new Vector2?(self.mainBodyChunk.pos - intVector.ToVector2() * 10f), intVector, Mathf.Lerp(1f, 1.5f, self.Adrenaline), eu);
+                return;
+            }
+            orig(self, grasp, eu);
         }
     }
 }
