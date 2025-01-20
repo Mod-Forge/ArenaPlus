b628882a58660cc9347507b391c77dc6d45f21de
Zeldack974 <115973286+Zeldack974@users.noreply.github.com>
2025-01-20 13:08:28 +0400

    feat: fun feature place holders

---
+++ src/Features/Fun/AllJokeRifle.cs
@@ -0,0 +1,33 @@
+﻿using ArenaPlus.Utils;
+using RWCustom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using MoreSlugcats;
+using ArenaPlus.Lib;
+
+namespace ArenaPlus.Features.Reworks
+{
+    [FeatureInfo(
+        id: "allJokeRifle",
+        name: "All Joke Rifle (WIP)",
+        category: BuiltInCategory.Spoilers,
+        description: "Replacer every spears with Joke Rifles",
+        enabledByDefault: false
+    )]
+    file class AllJokeRifle(FeatureInfoAttribute featureInfo) : Feature(featureInfo)
+    {
+        protected override void Register()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Unregister()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
