e7b624bca020b1f0a02f19baebe64dc6c73d3d79
Zeldack974 <115973286+Zeldack974@users.noreply.github.com>
2026-04-30 17:37:58 +0400

    feat 2.2.11?

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -27,7 +27,7 @@ using ArenaPlus.Features;
 
 namespace ArenaPlus
 {
-    [BepInPlugin(MOD_ID, "ArenaPlus", "2.2.10")]
+    [BepInPlugin(MOD_ID, "ArenaPlus", "2.2.11")]
     class Plugin : BaseUnityPlugin
     {
         internal const string MOD_ID = "modforge.ArenaPlus";
